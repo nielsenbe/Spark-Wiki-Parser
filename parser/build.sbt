b8cbb30a6cb35f700f61201286c7c493738ecf2a
nielsenbe <brad.e.nielsen@gmail.com>
2019-02-07 17:57:43 -0500

    Deleted Old Code

--- parser/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "test4"
-
-version := "0.1"
-
-scalaVersion := "2.12.8"
-        
--- parser/src/main/resources/wkp_text.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TEMPORARY VIEW wkp_text
-AS
-SELECT 
-    parentPageId        AS parent_page_id,	
-    parentRevisionId    AS parent_revision_id,	
-    parentHeaderId      AS parent_header_id,
-    text                AS text,
-    LENGTH(text)        AS text_length
-FROM texts
